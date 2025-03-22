a984e4d930d432d3092e30cc5a3b179ae0424cfe
cristiangodoyangel <creacciones@gmail.com>
2025-03-21 23:01:37 -0300

    feat: agregados nuevos servicios - Armado PC y Formateo Windows

--- src/main/java/com/drpcgamer/model/ServiceType.java
+++ src/main/java/com/drpcgamer/model/ServiceType.java
@@ -4,7 +4,11 @@ public enum ServiceType {
     NOTEBOOK_HOME("Mantención Notebook Gamer a Domicilio", 20000),
     NOTEBOOK_WORKSHOP("Mantención Notebook Gamer en Taller", 15000),
     DESKTOP_HOME("Mantención Desktop Gamer a Domicilio", 30000),
-    DESKTOP_WORKSHOP("Mantención Desktop Gamer en Taller", 25000);
+    DESKTOP_WORKSHOP("Mantención Desktop Gamer en Taller", 25000),
+    BUILD_HOME("Armado Completo PC Gamer a Domicilio", 35000),
+    BUILD_WORKSHOP("Armado Completo PC Gamer en Taller", 30000),
+    FORMAT_HOME("Formateo e Instalación Windows a Domicilio", 20000),
+    FORMAT_WORKSHOP("Formateo e Instalación Windows en Taller", 15000);
 
     private final String description;
     private final int price;
